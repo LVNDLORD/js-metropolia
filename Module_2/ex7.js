4dd8424b65b28a7dfb0485ad6eb294e4c19b6f4a
Andrey <andydeshko@gmail.com>
2022-11-17 18:50:26 +0200

    ex7 done

---
+++ Module_2/ex7.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const ul = document.createElement('ul');
+ul.setAttribute('id', 'target');
+document.body.appendChild(ul);
+const target = document.getElementById('target');
+
+const sides = Number(prompt('Enter number of sides on the dice: '));
+const randomNum = (sides) => Math.floor((Math.random() * sides + 1));
+
+while (true) {
+    let random = randomNum(sides);
+    if (random === sides) {
+        target.innerHTML += `<li>${random}</li>`;
+        break;
+    } else {
+        target.innerHTML += `<li>${random}</li>`;
+        continue;
+    }
+}
