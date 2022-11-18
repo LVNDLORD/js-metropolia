7743499c967f64ec07d9a0d93cdf215dffaa493e
Andrey <andydeshko@gmail.com>
2022-11-18 16:27:54 +0200

    modified ex1 according to the requirements in docs

--- Module_3/1.js
+++ Module_3/1.js
@@ -2,15 +2,7 @@
 
 const target = document.getElementById('target');
 target.setAttribute('class', 'my-list');
-let items = ['First item', 'Second item', 'Third Item'];
-let li = [];
+target.innerHTML += "<li>First item</li>";
+target.innerHTML += "<li>Second item</li>";
+target.innerHTML += "<li>Third item</li>";
 
-const createList = function (numListItems) {
-    for (let i = 0; i < numListItems; i++) {
-        li[i] = document.createElement('li');
-        target.appendChild(li[i]);
-        li[i].innerHTML = `${items[i]}`;
-    }
-}
-
-createList(3);
