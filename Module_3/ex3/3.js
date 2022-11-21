108eb5619202202ef38491ac64e367ea2e5216cf
Andrey <andydeshko@gmail.com>
2022-11-21 11:27:19 +0200

    modified ex3 according to the requirements in docs

--- Module_3/ex3/3.js
+++ Module_3/ex3/3.js
@@ -2,10 +2,8 @@
 
 const target = document.getElementById('target');
 const names = ['John', 'Paul', 'Jones'];
-let li = [];
 
 for (let i = 0; i < names.length; i++) {
-    li[i] = document.createElement('li');
-    target.appendChild(li[i]);
-    li[i].appendChild(document.createTextNode(`${names[i]}`));
+    target.innerHTML += `<li>${names[i]}</li>`;
 }
+
